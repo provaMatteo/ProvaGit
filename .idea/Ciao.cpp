cfac292e47921f959c37c58d2242407ccdba9144
Matteo <matteocatalano@protonmail.com>
2022-07-11 22:32:00 +0200

    CIao

---
+++ .idea/Ciao.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Catalano on 11/07/22.
+//
+
+#include "Ciao.h"
---
+++ .idea/Ciao.h
@@ -0,0 +1,16 @@
+//
+// Created by Matteo Catalano on 11/07/22.
+//
+
+#ifndef PROVAGIT_CIAO_H
+#define PROVAGIT_CIAO_H
+
+
+
+class Ciao {
+
+};
+
+
+
+#endif //PROVAGIT_CIAO_H
